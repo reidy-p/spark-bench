7e49f413d8ef2d210380be115197c5a0d640a0ef
Paul Reidy <paul_reidy@outlook.com>
2019-05-19 22:17:27 +0100

    Update GCP scripts

--- scripts/gcp/gcp-scp-commands.sh
+++ scripts/gcp/gcp-scp-commands.sh
@@ -1,4 +1,4 @@
-#gcloud compute scp ./target/assembly/*.jar $1-m:~/spark-bench-custom/target/assembly/
+gcloud compute scp ./target/assembly/*.jar $1-m:~/spark-bench-custom/target/assembly/
 gcloud compute scp ./bin/spark-bench.sh $1-m:~/spark-bench-custom/bin/
 gcloud compute scp ./scripts/spark-bench/* $1-m:~
 
--- scripts/gcp/webInterfaces.sh
+++ scripts/gcp/webInterfaces.sh
@@ -1,1 +1,1 @@
-"/mnt/c/Program Files (x86)/Google/Chrome/Application/chrome.exe" --proxy-server=socks5://localhost:8088 --user-data-dir=/tmp/$1-m
+"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --proxy-server="socks5://localhost:8088" --user-data-dir=/tmp/$1-m
